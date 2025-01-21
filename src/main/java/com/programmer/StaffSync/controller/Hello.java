7cec6caba42de05592a84f517a5aeed207f48fa4
NishantTheProgrammer <nishant@dhwaniris.com>
2025-01-21 12:44:01 +0530

    added test controller Hello

---
+++ src/main/java/com/programmer/StaffSync/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.programmer.StaffSync.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    
+    @GetMapping("hi")
+    public String hi() {
+        return "HI";
+    }
+}
