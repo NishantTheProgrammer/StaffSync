b83becb942210e649de8a7204d5b83da8c21e928
NishantTheProgrammer <nishant@dhwaniris.com>
2025-01-23 22:23:47 +0530

    added DockerFile

--- src/main/java/com/programmer/StaffSync/controller/CompanyController.java
+++ src/main/java/com/programmer/StaffSync/controller/CompanyController.java
@@ -5,10 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.programmer.StaffSync.entity.Company;
 import com.programmer.StaffSync.service.CompanyService;
