d2902db7e649f87c3e4f8c994b122071bb68b133
NishantTheProgrammer <nishant@dhwaniris.com>
2025-01-26 21:04:19 +0530

    added UserRepository

--- src/main/java/com/programmer/StaffSync/controller/Hello.java
+++ src/main/java/com/programmer/StaffSync/controller/Hello.java
@@ -1,13 +1,26 @@
 package com.programmer.StaffSync.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.programmer.StaffSync.entity.User;
+import com.programmer.StaffSync.repository.UserRepository;
+
 @RestController
 public class Hello {
+
+
+    @Autowired
+    private UserRepository userRepository;
     
     @GetMapping("hi")
     public String hi() {
         return "HI";
+    }
+
+    @GetMapping("/usertest") 
+    public User getUser() {
+        return this.userRepository.findByEmail("nishant@dhwaniris.com").get();
     }
 }
---
+++ src/main/java/com/programmer/StaffSync/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.programmer.StaffSync.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.programmer.StaffSync.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
