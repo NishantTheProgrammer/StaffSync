266c88cb235e63c40f6f3128c16dfa9ff88452cb
NishantTheProgrammer <nishant@dhwaniris.com>
2025-02-25 23:32:25 +0530

    added company and user association

--- src/main/java/com/programmer/StaffSync/entity/Company.java
+++ src/main/java/com/programmer/StaffSync/entity/Company.java
@@ -1,10 +1,14 @@
 package com.programmer.StaffSync.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
@@ -20,5 +24,24 @@ public class Company {
     private String logo;
     private String address;
     private String pan;
+
+
+    @ManyToOne
+    @JsonIgnore
+    private User user;
+
+    @JsonProperty("userId")
+    public Integer getUserId() {
+        return user != null ? user.getId() : null;
+    }
+
+    public void setUserId(Integer userId) {
+        if (userId != null) {
+            User user = new User();
+            user.setId(userId);  // Set user object based on userId
+            this.user = user;    // Set the user object
+        }
+    }
+
     
 }
